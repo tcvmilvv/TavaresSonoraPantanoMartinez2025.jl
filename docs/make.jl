3e53831401741f1f34e31507cbed3e869efb265d
Camila Tavares <tavares.camila@outlook.com>
2025-10-04 11:11:06 -0300

    Updated make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,6 @@ using TavaresSonoraPantanoMartinez2025
 makedocs(
     sitename = "TavaresSonoraPantanoMartinez2025",
     format = Documenter.HTML(
-        prettyurls = get(ENV, "CI", nothing) == "true",
         logo = "assets/zikv.svg",
     ),
     modules = [TavaresSonoraPantanoMartinez2025],
