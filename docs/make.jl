15ed19265c522aa5a8a68fc762bbca4e1829f3a5
Camila Tavares <tavares.camila@outlook.com>
2025-10-04 11:12:28 -0300

    Updated make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,15 +3,13 @@ using TavaresSonoraPantanoMartinez2025
 
 makedocs(
     sitename = "TavaresSonoraPantanoMartinez2025",
-    format = Documenter.HTML(
-        logo = "assets/zikv.svg",
-    ),
-    modules = [TavaresSonoraPantanoMartinez2025],
-    pages = [
-        "Home" => "index.md",
-    ]
+    format = Documenter.HTML(),
+    modules = [TavaresSonoraPantanoMartinez2025]
 )
 
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
 deploydocs(
     repo = "github.com/tcvmilvv/TavaresSonoraPantanoMartinez2025.jl"
 )
